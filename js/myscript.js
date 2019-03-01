6747f8c6e6ab47b778f0bdaee73fc1ac4cd443e8
Miller Bath <millerzbath@gmail.com>
2019-02-28 18:24:57 -0600

    Added dummy text page and the slideshow functionality.

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,5 @@
-function validate()
+
+function validate() //checks the two passwords
 {
   let p1 = document.getElementById("PW1").value;
   let p2 = document.getElementById("PW2").value;
@@ -12,4 +13,46 @@ function validate()
   }
 
 
-} //checks the two passwords
+}
+
+let counter = 0;
+function counterCheckPos()
+{
+  if(counter > imgArray.length-1)
+  {
+    counter = 0;
+  }
+}
+
+function counterCheckNeg()
+{
+  if (counter < 0)
+  {
+    counter = imgArray.length-1;
+  }
+}
+
+
+function prev()
+{
+  let img = document.getElementById("imgTag");
+  counter--;
+  counterCheckNeg();
+  img.src = imgArray[counter];
+}
+
+function fwd()
+{
+  let img = document.getElementById("imgTag");
+  counter++;
+  counterCheckPos();
+  img.src = imgArray[counter];
+}
+
+let imgArray = [
+  "https://www.aquinas-sta.org/uploaded/faculty_photos/28000036.jpg",
+  "https://www.aquinas-sta.org/uploaded/faculty_photos/28011721.jpg",
+  "https://www.aquinas-sta.org/uploaded/faculty_photos/28000014.jpg",
+  "https://www.aquinas-sta.org/uploaded/faculty_photos/28000024.jpg",
+  "https://bobcat.grahamdigital.com/image/upload/view?width=1280&height=720&method=crop&url=https://media.local10.com/photo/2019/01/15/Patrick-O%27Dea_1547573276765_17505875_ver1.0_640_360.jpg",
+]
